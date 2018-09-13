76bd3967b03ee9dca9da4ba0d172f6f23efee670
Miguel <miguel2105@mail.fresnostate.edu>
2018-09-13 00:07:49 -0700

    added a ! to the methods

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ var MyFirstProgram = /** @class */ (function () {
         console.log(argArray);
     };
     MyFirstProgram.prototype.runFirst = function (callback) {
-        console.log('this method runs first...');
+        console.log('this method runs first!...');
     };
     MyFirstProgram.prototype.runLast = function () {
-        console.log('this method runs last');
+        console.log('this method runs last!');
     };
     return MyFirstProgram;
 }());
--- main.ts
+++ main.ts
@@ -8,10 +8,10 @@ class MyFirstProgram {
         console.log(argArray);
     }
     runFirst(callback: Function) {
-        console.log('this method runs first...')
+        console.log('this method runs first!...')
     }
     runLast() {
-        console.log('this method runs last')
+        console.log('this method runs last!')
     }
 
 }
