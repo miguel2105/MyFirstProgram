c2a91d23f02199ff7dae58fa1b0c34ae1b1b88d1
Miguel <miguel2105@mail.fresnostate.edu>
2018-09-12 22:54:32 -0700

    first commit

--- main.ts
+++ main.ts
@@ -2,17 +2,18 @@
 
 class MyFirstProgram {
     constructor(...argArray: Array<any>) {
+        this.printArgs(argArray);
     }
-    this.printArgs(argArray);
-
     printArgs(argArray) {
         console.log(argArray);
     }
-    runFirst() {
+    runFirst(callback: Function) {
         console.log('this method runs first...')
     }
     runLast() {
         console.log('this method runs last')
     }
+
 }
-const newInstance = new MyFirstProgram("a","b","c");
+
+const newInstance = new MyFirstProgram("a", "b", "c");
